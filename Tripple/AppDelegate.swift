44fbc1c08e8e021557ef05b84c327944d6456ec0
Jonah Witcig <jwitcig@gmail.com>
2017-12-17 16:41:37 -0600

    .

--- Tripple/AppDelegate.swift
+++ Tripple/AppDelegate.swift
@@ -14,6 +14,8 @@ import Firebase
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
+    var window: UIWindow?
+
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         return true
     }
